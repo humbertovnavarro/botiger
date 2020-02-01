c262cfc67000cffd49b47c0b1e33c9fd151a84ad
kamaii <humbynav@gmail.com>
2020-01-31 23:56:39 -0800

    yamlhandler overhaul and bug fixes

---
+++ src/main/java/com/kamaii/github/io/botigers/commandListender.java
@@ -0,0 +1,16 @@
+package com.kamaii.github.io.botigers;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/*
+    Listens for chat commands and executes said commands.
+ */
+public class commands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+
+    }
+}
