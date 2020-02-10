f78ec70d3326b570cb143b1b5cc28484633f851b
kamaii <humbynav@gmail.com>
2020-02-10 14:31:00 -0800

    Fixed botiger db

--- src/main/java/com/kamaii/github/io/botigers/Botigers.java
+++ src/main/java/com/kamaii/github/io/botigers/Botigers.java
@@ -1,19 +1,25 @@
 package com.kamaii.github.io.botigers;
 
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Botigers extends JavaPlugin  implements Listener {
+    //Where we save the file.
     public static final File FileConfigurationPath = new File("./plugins/botigers/botigers.yml");
     private configHandler cfg;
     private List<botiger> botigerList;
@@ -28,25 +34,40 @@ public final class Botigers extends JavaPlugin  implements Listener {
     }
     @Override
     public void onDisable() {
-    }
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        log("What the fuck?");
+        log("Shutting down botigers");
+        try{
+            db.save(FileConfigurationPath);
+        }
+        catch(IOException ex){
+            log("Error saving botigers.yml to " + FileConfigurationPath);
+        }
     }
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        //Notice me senpai uwu
+        Player eventPlayer = event.getPlayer();
         Entity clicked = event.getRightClicked();
-        if(clicked.toString().equals("CraftVillager")){
-            if(clicked.getCustomName() != null){
-                if(db.getBoolean(event.getRightClicked().getCustomName().toString())){
 
+        if(clicked.toString().equals("CraftVillager")){
+            //If the villager has a custom name
+            if(clicked.getCustomName() != null){
+                //If this a botiger in the db
+                if(db.getBoolean(clicked.getCustomName().toString())){
+                    //If the uuid of the botigers player matches event player
+                    if(db.getString(clicked.getCustomName().toString() + ".player").equals(eventPlayer.getUniqueId().toString()) ){
+                        //open botiger interact menu.
+                    }
                 }
             }
-            else if(((Villager)event.getRightClicked()).getVillagerLevel() >= 5){
-                if(event.getPlayer().isSneaking()){
-                    int id = db.getInt(event.getPlayer().getUniqueId().toString() + "botigerCount");
+            //If it is not a botiger
+            else if(((Villager)clicked).getVillagerLevel() >= 5){
+                //If player is sneaking and villager is master level, add them to the worker list if player below worker limit
+                if(eventPlayer.isSneaking()){
+                    int id = db.getInt(eventPlayer.getUniqueId().toString() + ".botigerCount");
                     if(id + 1 < cfg.botigerMax) {
-                        botigerList.add(new botiger(event.getPlayer(),(Villager)event.getRightClicked(),db);
+                        botigerList.add(new botiger(eventPlayer,(Villager)clicked,db));
+                        db.set(eventPlayer.getUniqueId().toString() + ".botigerCount", id + 1);
+                        //Lets also save the config to prevent any server crashing mishaps
                         try{
                             this.getConfig().save(FileConfigurationPath);
                         }catch(Exception ex){
@@ -55,13 +76,26 @@ public final class Botigers extends JavaPlugin  implements Listener {
 
                     }
                     else{
-                        event.getPlayer().sendMessage("You have too many botigers.");
+                        eventPlayer.sendMessage("You have too many botigers.");
                     }
                 }else {
-                    event.getPlayer().sendMessage("This villager is available for hire.");
+                    Villager trader = (Villager)clicked;
+                    eventPlayer.sendMessage("This villager is available for hire.");
+
                 }
             }else{
-                event.getPlayer().sendMessage("This villager is level " + ((Villager) event.getRightClicked()).getVillagerLevel());
+                eventPlayer.sendMessage("This villager is level " + ((Villager) clicked).getVillagerLevel());
+            }
+        }
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+    }
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event){
+        for(int i = 0; i < botigerList.size(); i ++){
+            if(botigerList.get(i).getPlayer(db).equals(event.getPlayer().getUniqueId().toString())){
+                botigerList.get(i).goSleep(db);
             }
         }
     }
--- src/main/java/com/kamaii/github/io/botigers/botiger.java
+++ src/main/java/com/kamaii/github/io/botigers/botiger.java
@@ -1,23 +1,28 @@
 package com.kamaii.github.io.botigers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 
+import java.util.List;
 import java.util.Random;
 
-public class botiger{
+public class botiger {
         //Names
         public static final String[] names = {
-                "Arla", "Beech","Javan" ,"Mac", "Kay", "Mcdowell", "Vijay" ,"Rudd",
-                "Theo" ,"Russell", "Humphrey", "Wynn", "Farhaan", "Reyna", "Jeanne","Ali",
-                "Shyam","Iles", "Rafael", "Hernandez", "Morwenna", "Jennings", "Shelly", "Bowen",
-                "Finnlay","Gamble", "Kyal", "Schaefer", "Sheena", "Esquivel", "Lily-May", "Le",
-                "Priscilla","Betts", "Millicent" , "Mellor", "Nikki","Rosa", "Notch", "Humberto", "Rigoberto", "Alberto",
-                "Carolyn","Thornton", "Greg", "Marquez", "Juan", "Carlos", "Mary", "Joseph", "Crawford", "Smith",
-                "Karen", "Rogers", "Nguyen" , "Lee", "Thorn", "Flash", "Gordan", "Thom","Yorke","Queen","Jonathan","Joestar",
-                "Star","Platinum","Advy","Wilson","Claymore","Rainbow", "Taco","Joe"
+                "Arla", "Beech", "Javan", "Mac", "Kay", "Mcdowell", "Vijay", "Rudd", "Alfred", "Alani",
+                "Theo", "Russell", "Humphrey", "Wynn", "Farhaan", "Reyna", "Jeanne", "Ali",
+                "Shyam", "Iles", "Rafael", "Hernandez", "Morwenna", "Jennings", "Shelly", "Bowen",
+                "Finnlay", "Gamble", "Kyal", "Schaefer", "Sheena", "Esquivel", "Lily-May", "Le",
+                "Priscilla", "Betts", "Millicent", "Mellor", "Nikki", "Rosa", "Notch", "Humberto", "Rigoberto", "Alberto",
+                "Carolyn", "Thornton", "Greg", "Marquez", "Juan", "Carlos", "Mary", "Joseph", "Crawford", "Smith",
+                "Karen", "Rogers", "Nguyen", "Lee", "Thorn", "Flash", "Gordan", "Thom", "Yorke", "Queen", "Jonathan", "Joestar",
+                "Star", "Platinum", "Advy", "Wilson", "Claymore", "Rainbow", "Taco", "Joe"
         };
         public int id;
         public String name;
@@ -26,81 +31,93 @@ public class botiger{
         public int bedTime;
         public int alarm;
         public int seekRange;
-        public int speed;
+        public int agility;
+        public int strength;
         public int intelligence;
         public int luck;
         public int affinity;
+        public int wisdom;
+        public int charisma;
         public float awakeTime;
         public boolean isAsleep;
-        public boolean pathFinding = false;
+        public int seekState;
         //Write
         public botiger(Player player, Villager target, FileConfiguration db) {
                 owner = player;
                 name = generateName();
                 isAsleep = target.isSleeping();
                 Random r = new Random();
-                intelligence = r.nextInt(10);
-                luck = r.nextInt(10);
+                intelligence = r.nextInt(10) + 1;
+                luck = r.nextInt(10) + 1;
+                strength = r.nextInt(10) + 1;
+                agility = r.nextInt(10) + 1;
+                wisdom = r.nextInt(10) + 1;
+                charisma = r.nextInt(10) + 1;
                 affinity = 10;
                 awakeTime = 0;
                 seekRange = 8;
                 bedTime = 17;
                 alarm = 0;
-                pathFinding = false;
                 target.setAI(false);
                 target.setCustomName(name);
                 target.setHealth(19);
                 target.setInvulnerable(true);
-                db.set(this.name,true);
-                db.set(this.name + ".affinity",affinity);
-                db.set(this.name + ".intelligence",intelligence);
-                db.set(this.name + ".awakeTime",awakeTime);
-                db.set(this.name + ".seekRange",seekRange);
-                db.set(this.name + ".affinity",affinity);
+                db.set(this.name + ".active", true);
+                db.set(this.name + ".agility",agility);
+                db.set(this.name + ".strength",strength);
+                db.set(this.name + ".wisdom",wisdom);
+                db.set(this.name + ".charisma",charisma);
+                db.set(this.name + ".luck", luck);
+                db.set(this.name + ".intelligence", intelligence);
+                db.set(this.name + ".awakeTime", awakeTime);
                 db.set(this.name + ".alarm", alarm);
-                db.set(this.name + ".awakeTime",awakeTime);
-                db.set(this.name + ".player",player.getUniqueId().toString());
-                db.set(this.name + ".pathfinding",false);
+                db.set(this.name + ".seekRange", seekRange);
+                db.set(this.name + ".affinity", affinity);
+                db.set(this.name + ".player", player.getUniqueId().toString());
         }
+
         //Read
-        public botiger(String _name, Villager target,FileConfiguration db){
-                String vilName = target.getCustomName().toString();
-                if(db.getString(vilName + ".player") != null){
-                       owner = Bukkit.getPlayer((String)db.get(vilName + "player"));
+        public botiger(String _name, Villager target, FileConfiguration db) {
+                if (db.getString(_name + ".player") != null) {
+                        owner = Bukkit.getPlayer((String) db.get(_name + "player"));
                 }
-                if(db.getString(vilName + ".player") != null ){
-                        owner = Bukkit.getPlayer(db.getString(vilName + "player"));
+                if (db.getString(_name + ".player") != null) {
+                        owner = Bukkit.getPlayer(db.getString(_name + "player"));
                 }
-                luck = db.getInt(vilName + ".intelligence");
                 name = _name;
-                isAsleep = target.isSleeping();
-                luck = db.getInt(vilName + ".intelligence");
-                affinity = db.getInt(vilName + ".affinity");
-                awakeTime = db.getInt(vilName + ".awakeTime");
-                seekRange = db.getInt(vilName + ".seekRange");
-                bedTime = db.getInt(vilName + ".bedTime");
-                alarm = db.getInt(vilName  + ".alarm");
-                pathFinding = false;
+                luck = db.getInt(_name + ".luck");
+                affinity = db.getInt(_name + ".affinity");
+                awakeTime = db.getInt(_name + ".awakeTime");
+                seekRange = db.getInt(_name + ".seekRange");
+                bedTime = db.getInt(_name + ".bedTime");
+                alarm = db.getInt(_name + ".alarm");
                 target.setAI(false);
                 target.setCustomName(name);
                 target.setHealth(20);
                 target.setInvulnerable(true);
         }
-        private void rollStats(botiger bot){
+
+        private void rollStats(botiger bot) {
 
         }
-        public void seek(){
-        }
-        public void goSleep() {
 
+        public void seek(Material mat) {
         }
-        public String generateName(){
+        public void goSleep(FileConfiguration db) {
+                
+        }
+        public String getPlayer(FileConfiguration db){
+                return  db.getString(name + ".player");
+        }
+        public String generateName() {
                 Random rand = new Random();
                 String name = "";
                 int r = 0;
                 r = rand.nextInt(names.length);
                 name += names[r] + " ";
+                r = rand.nextInt(names.length);
                 name += names[r] + " ";
+                r = rand.nextInt(names.length);
                 name += names[r];
                 return name;
         }
